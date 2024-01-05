9f575fa18ba28b7ce3a21b25e7a66bdb79e806d5
hector <hectorrrivera1010@gmail.com>
2024-01-04 21:06:16 -0600

    [add] github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  site: "/personal-website-2024",
+  base: "https://hctor11.github.io"
 });
