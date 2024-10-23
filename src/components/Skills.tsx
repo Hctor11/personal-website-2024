6c7039c227965e700ed1fdd36b262150cb695126
hector <hectorrrivera1010@gmail.com>
2024-10-22 23:44:35 -0600

    changed skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -16,15 +16,8 @@ const Skills = () => {
   return (
     <div className="">
       <div className="webskills overflow-hidden">
-        <Paragraph
-          isTitle={true}
-          delay={0.2}
-          duration={0.6}
-          title="Web Development"
-          content=""
-        />
         <motion.div
-          className="technologies flex flex-col items-start md:flex-row md:justify-around overflow-hidden"
+          className="technologies flex flex-col items-center md:flex-col md:justify-around overflow-hidden"
           initial={{ y: "-200%", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{
@@ -34,19 +27,27 @@ const Skills = () => {
           }}
         >
           <IconMap icons={frontendIcons} label="FrontEnd" />
+        </motion.div>
+      </div>
+
+      <div className="webskills overflow-hidden">
+        <motion.div
+          className="technologies flex flex-col items-center md:flex-col md:justify-around overflow-hidden"
+          initial={{ y: "-200%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 1,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1],
+          }}
+        >
           <IconMap icons={backendIcons} label="BackEnd" />
         </motion.div>
       </div>
+
       <div className="database overflow-hidden">
-        <Paragraph
-          isTitle={true}
-          delay={0.2}
-          duration={0.6}
-          title="Data Base"
-          content=""
-        />
         <motion.div
-          className="technologies flex flex-col items-start md:flex-row md:justify-around overflow-hidden"
+          className="technologies flex flex-col items-center md:flex-col md:justify-around overflow-hidden"
           initial={{ y: "-200%", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{
@@ -56,9 +57,26 @@ const Skills = () => {
           }}
         >
           <IconMap icons={relDatabases} label="Relational" />
+        </motion.div>
+      </div>
+
+      <div className="database overflow-hidden">
+        <motion.div
+          className="technologies flex flex-col items-center md:flex-col md:justify-around overflow-hidden"
+          initial={{ y: "-200%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 1,
+            delay: 0.7,
+            ease: [0, 0.71, 0.2, 1],
+          }}
+        >
           <IconMap icons={noRelDB} label="No Relational" />
         </motion.div>
       </div>
+
+
+
       {/* <div className="datascience overflow-hidden">
         <Paragraph
           isTitle={true}
@@ -82,24 +100,18 @@ const Skills = () => {
         </motion.div>
       </div> */}
       <div className="design overflow-hidden">
-        <Paragraph
-          isTitle={true}
-          delay={0.2}
-          duration={0.8}
-          title="Design"
-          content=""
-        />
+
         <motion.div
           className="pb-10 technologies flex flex-col items-start md:flex-row md:justify-around overflow-hidden"
           initial={{ y: "-200%", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{
             duration: 1,
-            delay: 1,
+            delay: 0.8,
             ease: [0, 0.71, 0.2, 1],
           }}
         >
-          <IconMap icons={design} label="" />
+          <IconMap icons={design} label="Design" />
         </motion.div>
       </div>
     </div>
